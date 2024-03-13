580e136ff4d8fdd2892f27a47fecf0cbab3b5568
eglahmilgo@gmail.com <eglah.chepngeno@student.moringaschool.com>
2024-03-13 22:45:48 +0300

    ensured user can login with either password or fingerprint

--- app/Http/Controllers/AuthManager.php
+++ app/Http/Controllers/AuthManager.php
@@ -33,34 +33,50 @@ class AuthManager extends Controller
         $request->validate([
             'name' => 'required',
             'email' => 'required',
-            'password' => 'required'
+            'password' => 'required_if:auth_method,password'
         ]);
 
         $credentials = $request->only('email', 'password');
-        if (Auth::attempt($credentials)) {
-            return redirect()->intended(route('home'));
+        if ($request->auth_method == 'fingerprint') {
+        } else {
+            if (Auth::attempt($credentials)) {
+                return redirect()->intended(route('home'));
+            }
         }
         return redirect(route('login'))->with("error", "Login details are not valid");
     }
 
-    public function registrationPost(Request $request)
-    {
+    public function registrationPost(Request $request){
         $request->validate([
             'name' => 'required',
-            'email' => 'required',
-            'password' => 'required'
-            
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+            'auth_method' => 'required' // Ensure the user selects an authentication method
         ]);
+        
 
         $data['name'] = $request->name;
         $data['email'] = $request->email;
-        $data['password'] = Hash::make($request->password);
+        $data['auth_method'] = $request->auth_method;
+
+        if ($request->auth_method == 'fingerprint') {
+            
+        } else {
+            $data['password'] = Hash::make($request->password);
+        }
+
         $user = User::create($data);
-        if(!$user){
+
+        if (!$user) {
             return redirect(route('registration'))->with("error", "Registration failed, try again");
         }
+
         return redirect(route('login'))->with("success", "Successful Registration, Login to access the app");
     }
+
+
+
+
 
     function logout(){
         Session::flush();
--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -21,6 +21,12 @@
     <label  class="form-label">Password</label>
     <input type="password" class="form-control" name="password">
   </div>
+  </div>
+        
+        <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="useFingerprint" name="useFingerprint">
+            <label class="form-check-label" for="useFingerprint">Use Fingerprint</label>
+        </div>
   <div class="mb-3 form-check">
     <input type="checkbox" class="form-check-input" id="exampleCheck1">
     <label class="form-check-label" for="exampleCheck1">Check me out</label>
